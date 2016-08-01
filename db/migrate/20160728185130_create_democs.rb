f85d1377ecec655e9f1a44b0fee5b2c2ccbc5e73
Jason Amerine <amerine.19@gmail.com>
2016-08-01 08:35:03 -0400

    first commit

---
+++ db/migrate/20160728185130_create_democs.rb
@@ -0,0 +1,9 @@
+class CreateDemocs < ActiveRecord::Migration
+  def change
+    create_table :democs do |t|
+      t.string :single_rec
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ test/models/democ_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DemocTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
