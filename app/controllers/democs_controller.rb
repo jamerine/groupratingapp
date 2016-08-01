24305a98e580682a6241009de31fb86a24cedf38
Jason Amerine <amerine.19@gmail.com>
2016-08-01 10:56:36 -0400

    Manual Import Function DEMOC

--- app/controllers/democs_controller.rb
+++ app/controllers/democs_controller.rb
@@ -1,10 +1,11 @@
 class DemocsController < ApplicationController
   def index
     @democs = Democ.all
+    @democs_count = Democ.count
   end
 
   def import
-    Democ.import(params[:file])
+    Democ.import_file(params[:file])
     redirect_to root_url, notice: "Democs imported."
   end
 end
