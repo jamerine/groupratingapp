02cfde4ca8a14d4a9698694ec04bda543597b41d
Jason Amerine <amerine.19@gmail.com>
2016-08-19 16:09:09 -0400

    resque stuff

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -8,4 +8,10 @@ Resque.before_fork do
       conn = ActiveRecord::Base.establish_connection
   end
 
-Resque.redis = Rails.env.production? ? $redis : "localhost:6379"
+  rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
+  rails_env = ENV['RAILS_ENV'] || 'development'
+
+  resque_config = YAML.load_file(rails_root + '/config/resque.yml')
+  Resque.redis = resque_config[rails_env]
+
+  Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
