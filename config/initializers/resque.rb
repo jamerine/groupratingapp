793da6e1678e7eb08629bb253055cf5dae9bf70a
Jason Amerine <amerine.19@gmail.com>
2016-08-23 10:56:48 -0400

    shit

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -8,16 +8,8 @@ Resque.before_fork do
       conn = ActiveRecord::Base.establish_connection
   end
 
+  rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
+  rails_env = ENV['RAILS_ENV'] || 'development'
 
-  namespace :resque do
-    task :setup do
-      require 'resque'
-      ENV['QUEUE'] = '*'
-
-      Resque.redis = 'localhost:6379' unless Rails.env == 'production'
-    end
-  end
-
-
-  desc "Alias for resque:work (To run workers on Heroku)"
-  task "jobs:work" => "resque:work"
+  resque_config = YAML.load_file(rails_root + '/config/resque.yml')
+  Resque.redis = resque_config[rails_env]
