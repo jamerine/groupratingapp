3799d0a81ee4fbcd0173e769a427fa7bc0667564
Jason Amerine <amerine.19@gmail.com>
2016-08-24 09:37:39 -0400

    jobs

---
+++ app/workers/manual_class_update_create.rb
@@ -0,0 +1,44 @@
+class ManualClassUpdateCreate
+  @queue = :manual_class_update_create
+
+  def self.perform(group_rating_id)
+    @group_rating = GroupRating.find_by(id: group_rating_id)
+    @group_rating.status = "Manual Classes Updating"
+    @group_rating.save
+    FinalManualClassGroupRatingAndPremiumProjection.find_each do |man_class_proj|
+        man_class_exp = FinalManualClassFourYearPayrollAndExpLoss.find_by(policy_number: man_class_proj.policy_number, manual_number: man_class_proj.manual_number, representative_number: man_class_proj.representative_number)
+        @policy_calculation = PolicyCalculation.find_by(policy_number: man_class_proj.policy_number, representative_number: man_class_proj.representative_number)
+        unless @policy_calculation.nil?
+          ManualClassCalculation.where(policy_number: man_class_proj.policy_number, manual_number: man_class_proj.manual_number, representative_number: man_class_proj.representative_number).update_or_create(
+            representative_number: man_class_exp.representative_number,
+            policy_number: man_class_exp.policy_number,
+            policy_calculation_id: @policy_calculation.id,
+            manual_number: man_class_exp.manual_number,
+            manual_class_four_year_period_payroll: man_class_exp.manual_class_four_year_period_payroll,
+            manual_class_expected_loss_rate: man_class_exp.manual_class_expected_loss_rate,
+            manual_class_base_rate: man_class_exp.manual_class_base_rate,
+            manual_class_expected_losses: man_class_exp.manual_class_expected_losses,
+            manual_class_limited_loss_rate: man_class_exp.manual_class_limited_loss_rate,
+            manual_class_limited_losses: man_class_exp.manual_class_limited_losses,
+            manual_class_industry_group: man_class_proj.manual_class_industry_group,
+            manual_class_industry_group_premium_total: man_class_proj.manual_class_industry_group_premium_total,
+            manual_class_current_estimated_payroll: man_class_proj.manual_class_current_estimated_payroll,
+            manual_class_industry_group_premium_percentage: man_class_proj.manual_class_industry_group_premium_percentage,
+            manual_class_modification_rate: man_class_proj.manual_class_modification_rate,
+            manual_class_individual_total_rate: man_class_proj.manual_class_individual_total_rate,
+            manual_class_group_total_rate: man_class_proj.manual_class_group_total_rate,
+            manual_class_standard_premium: man_class_proj.manual_class_standard_premium,
+            manual_class_estimated_group_premium: man_class_proj.manual_class_estimated_group_premium,
+            manual_class_estimated_individual_premium: man_class_proj.manual_class_estimated_individual_premium,
+            data_source: man_class_proj.data_source
+          )
+        end
+    end
+    @group_rating = GroupRating.find_by(id: group_rating_id)
+    @group_rating.status = "Completed"
+    @group_rating.save
+    Resque.enqueue(PayrollUpdateCreate, group_rating_id)
+
+  end
+
+end
---
+++ app/workers/policy_update_create_single.rb
@@ -0,0 +1,74 @@
+class PolicyUpdateCreate
+  @queue = :policy_update_create
+
+  def self.perform(group_rating_id, policy_calculation_id)
+    @group_rating = GroupRating.find_by(id: group_rating_id)
+    @group_rating.status = "Policies Updating"
+    @group_rating.save
+    FinalPolicyExperienceCalculation.find_each(conditions: id: policy_calculation_id) do |policy_exp|
+        policy_proj = FinalPolicyGroupRatingAndPremiumProjection.find_by(policy_number: policy_exp.policy_number, representative_number: policy_exp.representative_number)
+        @policy_demographic = FinalEmployerDemographicsInformation.find_by(policy_number: policy_exp.policy_number, representative_number: policy_exp.representative_number)
+        @policy_calculation = PolicyCalculation.where(policy_number: policy_exp.policy_number, representative_number: policy_exp.representative_number).update_or_create(
+          representative_number: policy_exp.representative_number,
+          policy_group_number: policy_exp.policy_group_number,
+          policy_status: policy_exp.policy_status,
+          policy_total_four_year_payroll: policy_exp.policy_total_four_year_payroll,
+          policy_credibility_group: policy_exp.policy_credibility_group,
+          policy_maximum_claim_value: policy_exp.policy_maximum_claim_value,
+          policy_credibility_percent: policy_exp.policy_credibility_percent,
+          policy_total_expected_losses: policy_exp.policy_total_expected_losses,
+          policy_total_limited_losses: policy_exp.policy_total_limited_losses,
+          policy_total_claims_count: policy_exp.policy_total_claims_count,
+          policy_total_modified_losses_group_reduced: policy_exp.policy_total_modified_losses_group_reduced,
+          policy_total_modified_losses_individual_reduced: policy_exp.policy_total_modified_losses_individual_reduced,
+          policy_group_ratio: policy_exp.policy_group_ratio,
+          policy_individual_total_modifier: policy_exp.policy_individual_total_modifier,
+          policy_individual_experience_modified_rate: policy_exp.policy_individual_experience_modified_rate,
+          policy_industry_group: policy_proj.policy_industry_group,
+          group_rating_qualification: policy_proj.group_rating_qualification,
+          group_rating_tier: policy_proj.group_rating_tier,
+          group_rating_group_number: policy_proj.group_rating_group_number,
+          policy_total_current_payroll: policy_proj.policy_total_current_payroll,
+          policy_total_standard_premium: policy_proj.policy_total_standard_premium,
+          policy_total_individual_premium: policy_proj.policy_total_individual_premium,
+          policy_total_group_premium: policy_proj.policy_total_group_premium,
+          policy_total_group_savings: policy_proj.policy_total_group_savings,
+          policy_group_fees:policy_proj.policy_group_fees,
+          policy_group_dues: policy_proj.policy_group_dues,
+          policy_total_costs:policy_proj.policy_total_costs,
+          successor_policy_number: @policy_demographic.successor_policy_number,
+          currently_assigned_representative_number: @policy_demographic.currently_assigned_representative_number,
+          federal_identification_number: @policy_demographic.federal_identification_number,
+          business_name: @policy_demographic.business_name,
+          trading_as_name: @policy_demographic.trading_as_name,
+          in_care_name_contact_name: @policy_demographic.in_care_name_contact_name,
+          address_1: @policy_demographic.address_1,
+          address_2: @policy_demographic.address_2,
+          city: @policy_demographic.city,
+          state: @policy_demographic.state,
+          zip_code: @policy_demographic.zip_code,
+          zip_code_plus_4: @policy_demographic.zip_code_plus_4,
+          country_code: @policy_demographic.country_code,
+          county: @policy_demographic.county,
+          policy_creation_date: @policy_demographic.policy_creation_date,
+          current_policy_status: @policy_demographic.current_policy_status,
+          current_policy_status_effective_date: @policy_demographic.current_policy_status_effective_date,
+          merit_rate: @policy_demographic.merit_rate,
+          group_code: @policy_demographic.group_code,
+          minimum_premium_percentage: @policy_demographic.minimum_premium_percentage,
+          rate_adjust_factor: @policy_demographic.rate_adjust_factor,
+          em_effective_date: @policy_demographic.em_effective_date,
+          regular_balance_amount: @policy_demographic.regular_balance_amount,
+          attorney_general_balance_amount: @policy_demographic.attorney_general_balance_amount,
+          appealed_balance_amount: @policy_demographic.appealed_balance_amount,
+          pending_balance_amount: @policy_demographic.pending_balance_amount,
+          advance_deposit_amount: @policy_demographic.advance_deposit_amount,
+          data_source: policy_exp.data_source)
+    end
+    @group_rating = GroupRating.find_by(id: group_rating_id)
+    @group_rating.status = "Policy Updates Complete"
+    @group_rating.save
+        Resque.enqueue(ManualClassUpdateCreate, group_rating_id)
+  end
+
+end
