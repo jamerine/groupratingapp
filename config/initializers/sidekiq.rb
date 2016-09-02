fd5490ed170a3cc0d1278537c5dc361b0d7073fb
Jason Amerine <amerine.19@gmail.com>
2016-09-02 07:17:17 -0400

    changed connection size

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url:  ENV["REDISCLOUD_URL"] }
+  config.redis = { url:  ENV["REDISCLOUD_URL"], size: 9 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url:  ENV["REDISCLOUD_URL"] }
+  config.redis = { url:  ENV["REDISCLOUD_URL"], size: 1 }
 end
