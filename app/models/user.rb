a7385987e4d07248b69c69120e48927eb135079c
Jason Amerine <amerine.19@gmail.com>
2016-09-06 07:02:54 -0400

    procfile changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   has_and_belongs_to_many :users
 end
