99119050ae616c798eeeb6bfa581c78f39b932a5
Jason Amerine <amerine.19@gmail.com>
2016-09-20 09:44:11 -0400

    Monday, September 19

--- app/models/manual_class_calculation.rb
+++ app/models/manual_class_calculation.rb
@@ -14,100 +14,97 @@ class ManualClassCalculation < ActiveRecord::Base
   end
 
   def recalculate_experience
-    @group_rating = GroupRating.find_by(process_representative: self.representative_number)
+    self.transaction do
+      @group_rating = GroupRating.find_by(process_representative: self.representative_number)
 
-    four_year_sum = self.payroll_calculations.where("manual_class_effective_date BETWEEN :experience_period_lower_date and :experience_period_upper_date",  experience_period_lower_date: @group_rating.experience_period_lower_date, experience_period_upper_date: @group_rating.experience_period_upper_date).sum(:manual_class_payroll)
+      manual_class_four_year_sum = self.payroll_calculations.where("manual_class_effective_date BETWEEN :experience_period_lower_date and :experience_period_upper_date",  experience_period_lower_date: @group_rating.experience_period_lower_date, experience_period_upper_date: @group_rating.experience_period_upper_date).sum(:manual_class_payroll)
 
-    current_payroll = self.payroll_calculations.where("manual_class_effective_date >= :current_payroll_period_lower_date", current_payroll_period_lower_date: @group_rating.current_payroll_period_lower_date).sum(:manual_class_payroll)
+      manual_class_current_payroll = self.payroll_calculations.where("manual_class_effective_date >= :current_payroll_period_lower_date", current_payroll_period_lower_date: @group_rating.current_payroll_period_lower_date).sum(:manual_class_payroll)
 
+      @bwc_base_rate = BwcCodesBaseRatesExpLossRate.find_by(class_code: self.manual_number)
 
-    update_attribute(:manual_class_four_year_period_payroll, four_year_sum)
+      manual_class_expected_losses = @bwc_base_rate.expected_loss_rate * manual_class_four_year_sum
 
-    @bwc_base_rate = BwcCodesBaseRatesExpLossRate.find_by(class_code: self.manual_number)
+      update_attributes(manual_class_expected_loss_rate: @bwc_base_rate.expected_loss_rate, manual_class_base_rate: @bwc_base_rate.base_rate, manual_class_expected_losses: manual_class_expected_losses, manual_class_current_estimated_payroll: manual_class_current_payroll, manual_class_four_year_period_payroll: manual_class_four_year_sum)
 
-    expected_losses = @bwc_base_rate.expected_loss_rate * four_year_sum
+      @policy_calculation = PolicyCalculation.find(self.policy_calculation_id)
 
-    update_attributes(manual_class_expected_loss_rate: @bwc_base_rate.expected_loss_rate, manual_class_base_rate: @bwc_base_rate.base_rate, manual_class_expected_losses: expected_losses, manual_class_current_estimated_payroll: current_payroll)
-
-    @policy_calculation = PolicyCalculation.find(self.policy_calculation_id)
-
-    four_year_sum = @policy_calculation.manual_class_calculations.sum(:manual_class_four_year_period_payroll)
-    policy_expected_losses = @policy_calculation.manual_class_calculations.sum(:manual_class_expected_losses)
-    policy_estimated_current_payroll =  @policy_calculation.manual_class_calculations.sum(:manual_class_current_estimated_payroll)
+      policy_four_year_sum = @policy_calculation.manual_class_calculations.sum(:manual_class_four_year_period_payroll)
+      policy_expected_losses = @policy_calculation.manual_class_calculations.sum(:manual_class_expected_losses)
+      policy_estimated_current_payroll =  @policy_calculation.manual_class_calculations.sum(:manual_class_current_estimated_payroll)
 
 
-    @policy_calculation.update_attributes(policy_total_expected_losses: policy_expected_losses, policy_total_four_year_payroll: four_year_sum, policy_total_current_payroll: policy_estimated_current_payroll )
+      @policy_calculation.update_attributes(policy_total_expected_losses: policy_expected_losses, policy_total_four_year_payroll: policy_four_year_sum, policy_total_current_payroll: policy_estimated_current_payroll )
 
 
-    @credibility_row = BwcCodesCredibilityMaxLoss.where("expected_losses >= :expected_losses", expected_losses: @policy_calculation.policy_total_expected_losses).min
+      @credibility_row = BwcCodesCredibilityMaxLoss.where("expected_losses >= :expected_losses", expected_losses: @policy_calculation.policy_total_expected_losses).min
 
-    if @policy_calculation.policy_total_expected_losses <= 2000
-      @credibility_row.credibility_group = 0
-      @credibility_row.expected_losses = 0
-      @credibility_row.credibility_percent = 0
-      @credibility_row.group_maximum_value = 0
-    elsif @policy_calculation.policy_total_expected_losses >= 1000000
-      @credibility_row = BwcCodesCredibilityMaxLoss.find_by(expected_losses: 1000000)
-    else
-      @credibility_row = BwcCodesCredibilityMaxLoss.find_by(credibility_group: (@credibility_row.credibility_group - 1))
-    end
-
-    @limited_loss_rate = BwcCodesLimitedLossRatio.find_by(industry_group: self.manual_class_industry_group, credibility_group: @policy_calculation.policy_credibility_group)
-
-    limited_loss_rate = @limited_loss_rate.limited_loss_ratio
-    limited_losses = self.manual_class_expected_losses * @limited_loss_rate.limited_loss_ratio
-
-    update_attributes(manual_class_limited_losses: limited_losses, manual_class_limited_loss_rate: limited_loss_rate)
-
-    policy_limited_losses = @policy_calculation.manual_class_calculations.sum(:manual_class_limited_losses)
-
-    @policy_calculation.update_attributes(policy_maximum_claim_value: @credibility_row.group_maximum_value)
-
-    @policy_calculation.update_attributes(policy_credibility_percent: @credibility_row.credibility_percent, policy_credibility_group: @credibility_row.credibility_group, policy_total_limited_losses: policy_limited_losses)
-
-    @claims = @policy_calculation.claim_calculations.where("claim_injury_date >= :experience_period_lower_date and claim_injury_date <= :experience_period_upper_date",  experience_period_lower_date: @group_rating.experience_period_lower_date, experience_period_upper_date: @group_rating.experience_period_upper_date)
-
-    if @claims.nil? || @policy_calculation.policy_total_expected_losses == 0
-      policy_total_modified_losses_group_reduced = 0
-
-      policy_total_modified_losses_individual_reduced = 0
-
-      policy_total_claims_count = 0
-    else
-      @claims.each do |claim|
-        claim.recalculate_experience
-      end
-
-      policy_total_modified_losses_group_reduced = @claims.sum(:claim_modified_losses_group_reduced)
-
-      policy_total_modified_losses_individual_reduced = @claims.sum(:claim_modified_losses_individual_reduced)
-
-      policy_total_claims_count = @claims.count
-    end
-
-      if @policy_calculation.policy_total_expected_losses == 0
-        policy_group_ratio = 0
+      if @policy_calculation.policy_total_expected_losses <= 2000
+        @credibility_row.credibility_group = 0
+        @credibility_row.expected_losses = 0
+        @credibility_row.credibility_percent = 0
+        @credibility_row.group_maximum_value = 0
+      elsif @policy_calculation.policy_total_expected_losses >= 1000000
+        @credibility_row = BwcCodesCredibilityMaxLoss.find_by(expected_losses: 1000000)
       else
-        policy_group_ratio = (policy_total_modified_losses_group_reduced / @policy_calculation.policy_total_expected_losses).round(6)
+        @credibility_row = BwcCodesCredibilityMaxLoss.find_by(credibility_group: (@credibility_row.credibility_group - 1))
       end
 
+      @limited_loss_rate = BwcCodesLimitedLossRatio.find_by(industry_group: self.manual_class_industry_group, credibility_group: @policy_calculation.policy_credibility_group)
 
-    policy_individual_total_modifier =
-        if @policy_calculation.policy_total_limited_losses == 0
-          0
-        else
-          (((@policy_calculation.policy_total_modified_losses_individual_reduced - @policy_calculation.policy_total_limited_losses ) / @policy_calculation.policy_total_limited_losses) * @policy_calculation.policy_credibility_percent).round(6)
+      limited_loss_rate = @limited_loss_rate.limited_loss_ratio
+      limited_losses = self.manual_class_expected_losses * limited_loss_rate
+
+      update_attributes(manual_class_limited_losses: limited_losses, manual_class_limited_loss_rate: limited_loss_rate)
+
+      policy_limited_losses = @policy_calculation.manual_class_calculations.sum(:manual_class_limited_losses)
+
+      @policy_calculation.update_attributes(policy_credibility_percent: @credibility_row.credibility_percent, policy_credibility_group: @credibility_row.credibility_group, policy_total_limited_losses: policy_limited_losses, policy_maximum_claim_value: @credibility_row.group_maximum_value)
+
+      @claims = @policy_calculation.claim_calculations.where("claim_injury_date >= :experience_period_lower_date and claim_injury_date <= :experience_period_upper_date",  experience_period_lower_date: @group_rating.experience_period_lower_date, experience_period_upper_date: @group_rating.experience_period_upper_date)
+
+      if @claims.nil? || @policy_calculation.policy_total_expected_losses == 0
+        policy_total_modified_losses_group_reduced = 0
+
+        policy_total_modified_losses_individual_reduced = 0
+
+        policy_total_claims_count = 0
+      else
+        @claims.each do |claim|
+          claim.recalculate_experience
         end
 
-   policy_individual_experience_modified_rate = (policy_individual_total_modifier + 1).round(6)
+        policy_total_modified_losses_group_reduced = @claims.sum(:claim_modified_losses_group_reduced)
 
-   @policy_calculation.update_attributes(policy_total_modified_losses_group_reduced: policy_total_modified_losses_group_reduced, policy_total_modified_losses_individual_reduced: policy_total_modified_losses_individual_reduced, policy_total_claims_count: policy_total_claims_count, policy_individual_total_modifier: policy_individual_total_modifier, policy_individual_experience_modified_rate: policy_individual_experience_modified_rate, policy_group_ratio: policy_group_ratio )
+        policy_total_modified_losses_individual_reduced = @claims.sum(:claim_modified_losses_individual_reduced)
 
-   self.recalculate_premium
+        policy_total_claims_count = @claims.count
+      end
 
+        if @policy_calculation.policy_total_expected_losses == 0
+          policy_group_ratio = 0
+        else
+          policy_group_ratio = (policy_total_modified_losses_group_reduced / @policy_calculation.policy_total_expected_losses).round(6)
+        end
+
+
+      policy_individual_total_modifier =
+          if @policy_calculation.policy_total_limited_losses == 0
+            0
+          else
+            (((@policy_calculation.policy_total_modified_losses_individual_reduced - @policy_calculation.policy_total_limited_losses ) / @policy_calculation.policy_total_limited_losses) * @policy_calculation.policy_credibility_percent).round(6)
+          end
+
+     policy_individual_experience_modified_rate = (policy_individual_total_modifier + 1).round(6)
+
+     @policy_calculation.update_attributes(policy_total_modified_losses_group_reduced: policy_total_modified_losses_group_reduced, policy_total_modified_losses_individual_reduced: policy_total_modified_losses_individual_reduced, policy_total_claims_count: policy_total_claims_count, policy_individual_total_modifier: policy_individual_total_modifier, policy_individual_experience_modified_rate: policy_individual_experience_modified_rate, policy_group_ratio: policy_group_ratio )
+
+     self.recalculate_premium
+   end
   end
 
   def recalculate_premium
+
     @policy_calculation = PolicyCalculation.find(self.policy_calculation_id)
 
     @policy_calculation.manual_class_calculations.each do |manual_class_calculation|
