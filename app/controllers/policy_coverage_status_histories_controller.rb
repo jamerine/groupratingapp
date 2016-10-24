539cadce32cb382cfc642ccae51ab2d49bbba5b6
Jason Amerine <amerine.19@gmail.com>
2016-10-24 12:36:48 -0400

    predecessor changes and industry_group changes

---
+++ app/controllers/policy_coverage_status_histories_controller.rb
@@ -0,0 +1,4 @@
+class PolicyCoverageStatusHistoriesController < ApplicationController
+  
+
+end
--- app/workers/predecessor_account_update_create_process.rb
+++ app/workers/predecessor_account_update_create_process.rb
@@ -6,13 +6,10 @@ class PredecessorAccountUpdateCreateProcess
   def perform(group_rating_id)
     @group_rating = GroupRating.find(group_rating_id)
     @representative = Representative.find(@group_rating.representative_id)
-    predecessor_policies = ExceptionTablePolicyCombinedRequestPayrollInfo.all
-    predecessor_policies.each do |predecessor|
-      Account.where(representative_id: @representative.id, policy_number_entered: predecessor.predecessor_policy_number).update_or_create(representative_id: @representative.id, policy_number_entered: predecessor.predecessor_policy_number, status: 2)
+    ExceptionTablePolicyCombinedRequestPayrollInfo.find_each do |predecessor|
+      @account = Account.where(representative_id: @representative.id, policy_number_entered: predecessor.predecessor_policy_number).update_or_create(representative_id: @representative.id, policy_number_entered: predecessor.predecessor_policy_number, status: 2, name: "Predecessor Account for #{predecessor.successor_policy_number}")
 
-      @account = Account.find_by(representative_id: @representative.id, policy_number_entered: predecessor.predecessor_policy_number)
-
-      PolicyCalculation.where(representative_id: @representative.id, policy_number: predecessor.predecessor_policy_number, account_id: @account.id).update_or_create(policy_number: predecessor.predecessor_policy_number, policy_type: predecessor.predecessor_policy_type, representative_id: @representative.id, representative_number: @representative.representative_number, account_id: @account.id, policy_status: 'COMB', business_name: 'Predecessor Policy')
+      PolicyCalculation.where(representative_id: @representative.id, policy_number: predecessor.predecessor_policy_number, account_id: @account.id).update_or_create(policy_number: predecessor.predecessor_policy_number, policy_type: predecessor.predecessor_policy_type, representative_id: @representative.id, representative_number: @representative.representative_number, account_id: @account.id, policy_status: 'COMB', business_name: "Predecessor Policy for #{predecessor.successor_policy_number}", address_1: 'Unknown address_1', address_2: 'Unknown address_2', city: 'unknown city' )
 
     end
   end
