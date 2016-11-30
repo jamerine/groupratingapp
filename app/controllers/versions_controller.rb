66c734f57af447f819165a239281f2c53811e48e
Jason Amerine <amerine.19@gmail.com>
2016-11-30 07:51:23 -0500

    updates to versioning

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -6,7 +6,10 @@ class VersionsController < ApplicationController
   def index
     @model_type = params[:item_type].constantize
     @model = @model_type.find(params[:item_id])
+
     @versions = @model.versions.order("created_at DESC").each.map{|v| [v.created_at, v.changeset, v.whodunnit]}
+    user_ids = @model.versions.each.map{|v| [v.whodunnit.to_i]}
+    @version_users = User.where(:id => user_ids)
   end
 
 end
