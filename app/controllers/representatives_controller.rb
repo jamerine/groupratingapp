e869d8f10bd04eeb0ae747d593f4494e3e18fbcb
Jason Amerine <amerine.19@gmail.com>
2016-12-13 08:10:39 -0500

    user_scoped before_action

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,10 +1,7 @@
 class RepresentativesController < ApplicationController
 
   def index
-    @representatives_users = RepresentativesUser.where(user_id: current_user.id).pluck(:representative_id)
-    @representatives = Representative.where(id: @representatives_users)
   end
-
 
   def show
     @representative = Representative.find(params[:id])
