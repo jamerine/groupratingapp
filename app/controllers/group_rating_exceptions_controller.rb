194885c36769e5a706c8bbc8cc2fede7e7fc7749
Jason Amerine <amerine.19@gmail.com>
2016-12-15 10:15:06 -0500

    Added filtering, account header, program and program history

--- app/controllers/group_rating_exceptions_controller.rb
+++ app/controllers/group_rating_exceptions_controller.rb
@@ -1,5 +1,16 @@
 class GroupRatingExceptionsController < ApplicationController
   def index
     @group_rating_exceptions = GroupRatingException.where(representative_id: @representatives)
+    if params[:search].present? && params[:representative_number].present? && @representatives.where(representative_number: (params[:representative_number]))
+      @representative = @representatives.find_by(representative_number: params[:representative_number])
+      @group_rating_exceptions = @group_rating_exceptions.where(representative_id: @representative.id).search(params[:search]).paginate(page: params[:page], per_page: 50)
+    elsif params[:search].present?
+      @group_rating_exceptions = @group_rating_exceptions.search(params[:search]).paginate(page: params[:page], per_page: 50)
+    elsif params[:representative_number].present? && @representatives.where(representative_number: (params[:representative_number]))
+      @representative = @representatives.find_by(representative_number: params[:representative_number])
+      @group_rating_exceptions = @group_rating_exceptions.where(representative_id: @representative.id).paginate(page: params[:page], per_page: 50)
+    else
+      @group_rating_exceptions = @group_rating_exceptions.paginate(page: params[:page], per_page: 50)
+    end
   end
 end
