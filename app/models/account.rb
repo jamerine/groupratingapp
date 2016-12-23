841982c2957b6f7ec0c7fa184a8ed8b973ede386
Jason Amerine <amerine.19@gmail.com>
2016-12-23 13:57:45 -0500

    fixed account

--- app/models/account.rb
+++ app/models/account.rb
@@ -192,7 +192,7 @@ class Account < ActiveRecord::Base
         # NEGATIVE PAYROLL ON A MANUAL CLASS
 
         if !self.policy_calculation.manual_class_calculations.where("manual_class_current_estimated_payroll < 0 or manual_class_four_year_period_payroll < 0").empty?
-          if self.group_rating_exceptions.where(exception_reason: 'manual_class_negative_payroll', resolved: true).empty
+          if self.group_rating_exceptions.where(exception_reason: 'manual_class_negative_payroll', resolved: true).empty?
             GroupRatingException.create(account_id: self.id, exception_reason: 'manual_class_negative_payroll', representative_id: self.representative_id)
           end
           GroupRatingRejection.create(account_id: self.id, reject_reason: 'manual_class_negative_payroll', representative_id: @group_rating.representative_id)
