3d3f1b8a87e816aa0a43377103217645da3f8520
Jason Amerine <amerine.19@gmail.com>
2017-01-10 12:42:44 -0500

    Email changes

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -38,11 +38,8 @@ class RepresentativesController < ApplicationController
   def export_manual_classes
     @representative = Representative.find(params[:representative_id])
     @manual_class_calculations = @representative.manual_class_calculations
-    Weekly159Request.perform_async(current_user.id, @representative.id)
 
-    flash[:notice] = "Your Weekly 159 request export is now being generated.  Please checkout your email for your generated file."
-    redirect_to @representative
-    
+
     # respond_to do |format|
     #   format.html
     #   format.csv { send_data @manual_class_calculations.to_csv, filename: "#{@representative.abbreviated_name}_manual_classes_#{Date.today}.csv" }
@@ -53,13 +50,15 @@ class RepresentativesController < ApplicationController
 
   def export_159_request_weekly
     @representative = Representative.find(params[:representative_id])
-    @accounts = @representative.accounts.where("weekly_request = true or request_date is null or status in (0,3,4)")
+    WeeklyRequest.perform_async(current_user.id, @representative.id)
 
+    flash[:notice] = "Your Weekly 159 request export is now being generated.  Please checkout your email for your generated file."
+    redirect_to @representative
 
-    respond_to do |format|
-      format.html
-      format.csv { send_data @accounts.to_request(@representative.representative_number), filename: "#{@representative.abbreviated_name}_159_request_#{Date.today}.csv" }
-    end
+    # respond_to do |format|
+    #   format.html
+    #   format.csv { send_data @accounts.to_request(@representative.representative_number), filename: "#{@representative.abbreviated_name}_159_request_#{Date.today}.csv" }
+    # end
   end
 
 
