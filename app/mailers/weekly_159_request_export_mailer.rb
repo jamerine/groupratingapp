d21b810d3ef44132ede3f63d6d4d617b28b1a99c
Jason Amerine <amerine.19@gmail.com>
2017-01-10 12:25:45 -0500

    procfile changes

---
+++ app/mailers/weekly_159_request_export_mailer.rb
@@ -0,0 +1,11 @@
+class Weekly159RequestExportMailer < ActionMailer::Base
+ default :from => "jason@dittoh.com"
+
+  def weekly_159_request_export(user, representative, csv_string)
+     attachments['report.csv'] = csv_string
+     @representative = representative
+     @user = user
+     mail(:to => @user.email, :subject => "#{ @representative.abbreviated_name} 159 Request Export")
+  end
+
+end
