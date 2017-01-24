a0fff32e611a7658b4cc25ea392e77012a90aa80
Jason Amerine <amerine.19@gmail.com>
2017-01-24 11:52:11 -0500

    account_program_import

--- app/models/account_program.rb
+++ app/models/account_program.rb
@@ -1,6 +1,7 @@
 class AccountProgram < ActiveRecord::Base
   belongs_to :account
 
-  enum program_type: [:group_rating, :group_retro, :retainer]
+  enum program_type: [:group_rating, :group_retro, :grow_ohio, :retainer]
 
+  enum status: [:accepted, :rejected, :void, :withdraw]
 end
---
+++ app/workers/account_program_import.rb
@@ -0,0 +1,18 @@
+class AccountProgramImport
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :import_file
+
+  def perform(hash)
+
+    @representative = Representative.find_by(representative_number: hash["representative_number"])
+    @account = Account.find_by(representative_id: @representative.id, policy_number_entered: hash["policy_number"] )
+
+    if @account
+      @account_program = AccountProgram.new(hash.except("representative_number", "policy_number"))
+      @account_program.assign_attributes(account_id: @account.id)
+      @account_program.save
+    end
+
+  end
+end
