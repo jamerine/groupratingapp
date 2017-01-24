b5f4f6621d25ef8737a878786d468adfc04775b6
Jason Amerine <amerine.19@gmail.com>
2017-01-24 14:11:21 -0500

    took off next

--- app/workers/group_rating_mark_complete.rb
+++ app/workers/group_rating_mark_complete.rb
@@ -10,20 +10,20 @@ class GroupRatingMarkComplete
     @group_rating.status = "Completed"
     @group_rating.save
 
-    if @group_rating.representative_id < @representatives_count
-      GroupRating.where(representative_id: (@group_rating.representative_id + 1)).destroy_all
-      @new_group_rating = GroupRating.new(experience_period_lower_date: @group_rating.experience_period_lower_date, experience_period_upper_date: @group_rating.experience_period_upper_date, current_payroll_period_lower_date: @group_rating.current_payroll_period_lower_date, current_payroll_period_upper_date: @group_rating.current_payroll_period_upper_date, representative_id: (@group_rating.representative_id + 1) )
-      @representative = Representative.find(@new_group_rating.representative_id)
-      @new_group_rating.process_representative = @representative.representative_number
-      @new_group_rating.status = 'Queuing'
-      if @new_group_rating.save
-        @import = Import.new(process_representative: @new_group_rating.process_representative, representative_id: @new_group_rating.representative_id, group_rating_id: @new_group_rating.id, import_status: 'Queuing', parse_status: 'Queuing')
-          # Flat files
-          if @import.save
-            ImportProcess.perform_in(2.minutes, @import.process_representative, @import.id, @representative.abbreviated_name, @new_group_rating.id)
-          end
-      end
-    end
+    # if @group_rating.representative_id < @representatives_count
+    #   GroupRating.where(representative_id: (@group_rating.representative_id + 1)).destroy_all
+    #   @new_group_rating = GroupRating.new(experience_period_lower_date: @group_rating.experience_period_lower_date, experience_period_upper_date: @group_rating.experience_period_upper_date, current_payroll_period_lower_date: @group_rating.current_payroll_period_lower_date, current_payroll_period_upper_date: @group_rating.current_payroll_period_upper_date, representative_id: (@group_rating.representative_id + 1) )
+    #   @representative = Representative.find(@new_group_rating.representative_id)
+    #   @new_group_rating.process_representative = @representative.representative_number
+    #   @new_group_rating.status = 'Queuing'
+    #   if @new_group_rating.save
+    #     @import = Import.new(process_representative: @new_group_rating.process_representative, representative_id: @new_group_rating.representative_id, group_rating_id: @new_group_rating.id, import_status: 'Queuing', parse_status: 'Queuing')
+    #       # Flat files
+    #       if @import.save
+    #         ImportProcess.perform_in(2.minutes, @import.process_representative, @import.id, @representative.abbreviated_name, @new_group_rating.id)
+    #       end
+    #   end
+    # end
 
 
   end
