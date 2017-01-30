f31a979158e8fda87332021061adb163859de684
Jason Amerine <amerine.19@gmail.com>
2017-01-30 12:20:27 -0500

    representative changes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_action :configure_sign_up_params, only: [:create]
+before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
     @representatives = Representative.all
+    @roles = User.roles
     super
   end
 
