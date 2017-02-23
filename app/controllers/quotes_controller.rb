aab6aef76b71894f9cef61cc7cea5a0fb6512aad
Jason Amerine <amerine.19@gmail.com>
2017-02-23 16:45:02 -0500

    report changes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -66,13 +66,13 @@ class QuotesController < ApplicationController
       format.html
       format.pdf do
         pdf = GroupRatingQuote.new(@quote, @account, @policy_calculation, view_context)
-        # send_data pdf.render, filename: "quote_#{@quote.id}.pdf",
-        #                       type: "application/pdf",
-        #                       disposition: "inline"
-        pdf.render_file "app/reports/quote_#{@quote.id}.pdf"
+        send_data pdf.render, filename: "quote_#{@quote.id}.pdf",
+                              type: "application/pdf",
+                              disposition: "inline"
+        # pdf.render_file "app/reports/quote_#{@quote.id}.pdf"
       end
     end
-    redirect_to edit_quote_path(@quote), notice: "Quote Generated"
+    # redirect_to edit_quote_path(@quote), notice: "Quote Generated"
   end
 
   private
