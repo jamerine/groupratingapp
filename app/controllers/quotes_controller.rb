3d4de492eb8b80c38d591795158a7671a63b1893
Jason Amerine <amerine.19@gmail.com>
2017-04-20 08:46:50 -0400

    rating-values

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -11,19 +11,66 @@ class QuotesController < ApplicationController
 
   def create
     @account = Account.find(params[:quote][:account_id])
+    @representative = @account.representative
+    @group_rating = @representative.group_ratings.last
     @program_types = Quote.program_types
     @quote = Quote.new(quote_params)
     @type = @program_types[params[:quote][:program_type]]
+    @policy_calculation = @account.policy_calculation
     if @quote.save
       @quote.update_attributes(quote_sent_date: @quote.created_at)
-      @quote.generate_invoice_number
+      policy_year = @quote.quote_year
+      s = "#{@account.policy_number_entered}-#{policy_year}-#{@quote.id}"
+      @quote.assign_attributes(invoice_number: s)
       ##### ADDED THIS PART
-      pdf = GroupRatingQuote.new(@quote, @account, @account.policy_calculation, view_context)
-      uploader = QuoteUploader.new
+      combine_pdf = CombinePDF.new
+
+      if params[:quote][:intro] == "1"
+        intro_pdf = ArmIntro.new(@quote, @account, @policy_calculation, view_context)
+        intro_pdf_render = intro_pdf.render
+        combine_pdf << CombinePDF.parse(intro_pdf_render)
+      end
+
+      if params[:quote][:quote] == "1"
+        quote_pdf = GroupRatingQuote.new(@quote, @account, @policy_calculation, view_context)
+        quote_pdf_render = quote_pdf.render
+        combine_pdf << CombinePDF.parse(quote_pdf_render)
+      end
+
+      if params[:quote][:ac_26] == "1"
+        ac_26_pdf = Ac26.new(@quote, @account, @policy_calculation, view_context)
+        ac_26_pdf_render = ac_26_pdf.render
+        combine_pdf << CombinePDF.parse(ac_26_pdf_render)
+      end
+
+      if params[:quote][:ac_2] == "1"
+        ac_2_pdf = Ac2.new(@quote, @account, @policy_calculation, view_context)
+        ac_2_pdf_render = ac_2_pdf.render
+        combine_pdf << CombinePDF.parse(ac_2_pdf_render)
+      end
+
+      if params[:quote][:quote] == "1"
+        contract_pdf = ArmContract.new(@quote, @account, @policy_calculation, view_context)
+        contract_pdf_render = contract_pdf.render
+        combine_pdf << CombinePDF.parse(contract_pdf_render)
+      end
+
+      if params[:quote][:questionnaire] == "1"
+        questionnaire_pdf = ArmQuestionnaire.new(@quote, @account, @policy_calculation, view_context)
+        questionnaire_pdf_render = questionnaire_pdf.render
+        combine_pdf << CombinePDF.parse(questionnaire_pdf_render)
+      end
+
+      if params[:quote][:invoice] == "1"
+        invoice_pdf = ArmInvoice.new(@quote, @account, @policy_calculation, view_context)
+        invoice_pdf_render = invoice_pdf.render
+        combine_pdf << CombinePDF.parse(invoice_pdf_render)
+      end
+
+      # uploader = QuoteUploader.new
       tmpfile = Tempfile.new(["#{ @account.policy_number_entered }_quote_#{ @quote.id }", '.pdf'])
       tmpfile.binmode
-      tmpfile.write (pdf.render)
-      # uploader.store! tmpfile
+      tmpfile.write(combine_pdf.to_pdf)
       @quote.quote_generated = tmpfile
       tmpfile.close
       tmpfile.unlink
