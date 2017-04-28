020b89a5e405557a1b5616e6bbd38384fa0bef58
Jason Amerine <amerine.19@gmail.com>
2017-04-28 09:45:43 -0400

    billing_info_quote

--- app/mailers/weekly_request_export_mailer.rb
+++ app/mailers/weekly_request_export_mailer.rb
@@ -2,10 +2,10 @@ class WeeklyRequestExportMailer < ActionMailer::Base
  default :from => "jason@dittoh.com"
 
   def weekly_request_export(user, representative, csv_string)
-     attachments['report.csv'] = csv_string
      @representative = representative
      @user = user
-     mail(:to => @user.email, :subject => "#{ @representative.abbreviated_name} 159 Request Export")
+     attachments["#{@representative.abbreviated_name}_159_request.txt"] = csv_string
+     mail(:to => @user.email, :subject => "#{ @representative.abbreviated_name} 159 Request Export File")
   end
 
 end
