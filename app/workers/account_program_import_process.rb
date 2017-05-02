9ce04f4ec8e4547678d75fa4c307187fbbc13151
Jason Amerine <amerine.19@gmail.com>
2017-05-02 10:23:30 -0400

    changes to account_program import worker

--- app/workers/account_program_import_process.rb
+++ app/workers/account_program_import_process.rb
@@ -1,13 +1,15 @@
+require 'open-uri'
 class AccountProgramImportProcess
   include Sidekiq::Worker
 
   sidekiq_options queue: :import_file
 
   def perform(file)
+    csv_file = open(file,'rb:UTF-8')
 
     begin
-      
-      CSV.foreach(file, headers: true) do |row|
+
+      CSV.foreach(csv_file, headers: true) do |row|
         hash = row.to_hash # exclude the price field
         AccountProgramImport.perform_async(hash)
         puts "Import successful."
