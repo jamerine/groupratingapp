3abed4245bf9c8506d3d9e0a793104dd3b781fa3
Jason Amerine <amerine.19@gmail.com>
2017-05-02 13:40:23 -0400

    Changes

--- app/models/account_program.rb
+++ app/models/account_program.rb
@@ -1,7 +1,7 @@
 class AccountProgram < ActiveRecord::Base
   belongs_to :account
 
-  enum program_type: [:group_rating, :group_retro, :retainer, :grow_ohio, :one_claim_program]
+  enum program_type: [:group_rating, :group_retro, :retainer, :grow_ohio, :one_claim_program, :em_cap]
 
   enum status: [:accepted, :rejected, :void, :withdraw]
 end
