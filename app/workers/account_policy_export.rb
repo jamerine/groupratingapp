bcc485ce0f0aebeb0e45d6d9e8826579e58e55ac
Jason Amerine <amerine.19@gmail.com>
2017-05-17 13:19:32 -0400

    changes to account export

--- app/workers/account_policy_export.rb
+++ app/workers/account_policy_export.rb
@@ -11,18 +11,23 @@ class AccountPolicyExport
 
     @accounts = Account.joins(:policy_calculation).select("accounts.*, policy_calculations.*").where("accounts.representative_id = ?", representative_id)
 
+
     attributes = Account.column_names
     PolicyCalculation.column_names.each do |p|
-      unless p == 'id' || p == 'representative_number' || p == 'policy_number' || p ==  'data_source' || p == 'created_at' || p == 'updated_at' || p == 'representative_id'|| p == 'account_id' || p == 'federal_identification_number'
+      # unless p == 'id' || p == 'representative_number' || p == 'policy_number' || p ==  'data_source' || p == 'created_at' || p == 'updated_at' || p == 'representative_id'|| p == 'account_id' || p == 'federal_identification_number'
         attributes << p
-      end
+      # end
     end
+    attributes << "reject_reason"
 
     csv_string = CSV.generate(headers: true) do |csv|
       csv << attributes
 
       @accounts.each do |account|
-        csv << attributes.map{ |attr| account.send(attr) }
+        # csv << attributes.map{ |attr| account.send(attr) }
+        csv_line = account.attributes.values
+        csv_line << account.group_rating_rejections.where(program_type: 'group_rating').pluck(:reject_reason).to_s
+        csv << csv_line
       end
       csv.close
     end
