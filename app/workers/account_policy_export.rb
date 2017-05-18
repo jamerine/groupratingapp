446b06868d50afca6538b87d40407d3ba4287e06
Jason Amerine <amerine.19@gmail.com>
2017-05-18 10:53:59 -0400

    changes to account export

--- app/workers/account_policy_export.rb
+++ app/workers/account_policy_export.rb
@@ -9,12 +9,15 @@ class AccountPolicyExport
     @representative = Representative.find(representative_id)
     # @accounts = Account.joins(:policy_calculation, :group_rating_rejections).select("accounts.*, policy_calculations.*, CASE count(group_rating_rejections.reject_reason) WHEN 0 THEN '' ELSE string_agg(group_rating_rejections.reject_reason, ' | ') END  as reject_reason").where("accounts.representative_id = ? and (group_rating_rejections.program_type = ? or group_rating_rejections.program_type is null)", representative_id, 'group_rating').group("accounts.id, policy_calculations.id")
 
-    @accounts = Account.joins(:policy_calculation).select("accounts.*, policy_calculations.*").where("accounts.representative_id = ?", representative_id)
+    @accounts = Account.joins(:policy_calculation).select("accounts.*, policy_calculations.*").where("accounts.representative_id = ?", representative_id).except("policy_calculations.updated_at, policy_calculations.account_id")
 
 
-    attributes = Account.column_names
+    attributes = Account.attribute_names
+
+
+
     PolicyCalculation.column_names.each do |p|
-      # unless p == 'id' || p == 'representative_number' || p == 'policy_number' || p ==  'data_source' || p == 'created_at' || p == 'updated_at' || p == 'representative_id'|| p == 'account_id' || p == 'federal_identification_number'
+      unless p == 'id' || p == 'federal_identification_number' || p == 'created_at' || p == 'updated_at' || p == 'account_id' ||
         attributes << p
       # end
     end
@@ -25,7 +28,7 @@ class AccountPolicyExport
 
       @accounts.each do |account|
         # csv << attributes.map{ |attr| account.send(attr) }
-        csv_line = account.attributes.values
+        csv_line = account.attributes.except("account_id, federal_identification_number").values
         csv_line << account.group_rating_rejections.where(program_type: 'group_rating').pluck(:reject_reason).to_s
         csv << csv_line
       end
