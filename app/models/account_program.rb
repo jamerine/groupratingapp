8f3b274dc83a9dedeb9257e5605afa135d0cd497
Jason Amerine <amerine.19@gmail.com>
2017-05-29 14:56:25 -0400

    changes to rates file processing

--- app/models/account_program.rb
+++ app/models/account_program.rb
@@ -4,4 +4,12 @@ class AccountProgram < ActiveRecord::Base
   enum program_type: [:group_rating, :group_retro, :retainer, :grow_ohio, :one_claim_program, :em_cap]
 
   enum status: [:accepted, :rejected, :void, :withdraw]
+
+  def self.update_or_create(attributes)
+    obj = first || new
+    obj.assign_attributes(attributes)
+    obj.save
+    obj
+  end
+  
 end
