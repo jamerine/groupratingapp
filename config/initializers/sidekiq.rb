c1c32881f3ea814be9b033e342cf467e1f76fa27
Jason Amerine <amerine.19@gmail.com>
2017-07-24 09:50:48 -0400

    removed error emails in sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 require 'sidekiq_calculations'
 require 'csv'
 require 'tempfile'
-require Rails.root + "lib/sidekiq_error_notifier"
+# require Rails.root + "lib/sidekiq_error_notifier"
 
 Sidekiq.configure_client do |config|
   sidekiq_calculations = SidekiqCalculations.new
@@ -22,5 +22,5 @@ Sidekiq.configure_server do |config|
     url: ENV['REDISCLOUD_URL']
   }
 
-  config.error_handlers << Proc.new {|exception, context_hash| SidekiqErrorNotifier.notify(exception, context_hash) }
+  # config.error_handlers << Proc.new {|exception, context_hash| SidekiqErrorNotifier.notify(exception, context_hash) }
 end
