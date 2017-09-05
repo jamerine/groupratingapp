a47a16d9c3d2c0b716ec7105a03de9a1b6f7725d
Jason Amerine <amerine.19@gmail.com>
2017-09-05 14:30:53 -0400

    changes

--- app/workers/group_rating_all_create.rb
+++ app/workers/group_rating_all_create.rb
@@ -266,10 +266,11 @@ class GroupRatingAllCreate
           @account.group_retro
       else
         @policy_demographic = PolicyCalculation.find_by(policy_number: policy_number, representative_number: process_representative)
+        # @policy_demographic = PolicyCalculation.find_by(policy_number: 1740451, representative_number: 1740)
         unless @policy_demographic.nil?
-          @account = Account.find_by(policy_number_entered: policy_number, representative_id: representative_id)
+          @account = Account.find_by(policy_number_entered: @policy_demographic.policy_number, representative_id: representative_id)
           @account.policy_calculation.manual_class_calculations.each do |manual_class|
-            ProcessPayrollAllTransactionsBreakdownByManualClass.where("reporting_period_start_date >= :reporting_period_start_date and representative_number = :representative_number and manual_number = :manual_number and policy_number = :policy_number",  reporting_period_start_date: experience_period_lower_date, representative_number: process_representative, manual_number: manual_class.manual_number, policy_number: @manual_class_calculation.policy_number).find_each do |payroll_transaction|
+            ProcessPayrollAllTransactionsBreakdownByManualClass.where("reporting_period_start_date >= :reporting_period_start_date and representative_number = :representative_number and manual_number = :manual_number and policy_number = :policy_number",  reporting_period_start_date: experience_period_lower_date, representative_number: process_representative, manual_number: manual_class.manual_number, policy_number: manual_class.policy_number).find_each do |payroll_transaction|
               unless manual_class.nil? || payroll_transaction.id.nil? || payroll_transaction.manual_number == 0
                 PayrollCalculation.where(representative_number: payroll_transaction.representative_number,
                 policy_number: payroll_transaction.policy_number,
