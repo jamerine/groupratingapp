073bbb6d1416a7f45c1439f37ea4532cb416e2d4
Jason Amerine <amerine.19@gmail.com>
2018-02-24 13:47:37 -0500

    fix to import

--- app/workers/account_program_import.rb
+++ app/workers/account_program_import.rb
@@ -4,13 +4,17 @@ class AccountProgramImport
   sidekiq_options queue: :import_file, retry: 1
 
   def perform(hash)
-
     @representative = Representative.find_by(representative_number: hash["representative_number"])
     @account = Account.find_by(representative_id: @representative.id, policy_number_entered: hash["policy_number"] )
 
     if @account
-      @account_program = AccountProgram.new(hash.except("representative_number", "policy_number"))
-      @account_program.assign_attributes(account_id: @account.id)
+      fees_amount = hash["fees_amount"].gsub(/[^\d^\.]/, '').to_f
+      program_type = hash["program_type"].to_s
+      group_code = hash["group_code"].to_s
+      quote_tier = hash["quote_tier"]
+      effective_start_date = hash["effective_start_date"].to_date
+      effective_end_date = hash["effective_end_date"].to_date
+      @account_program = AccountProgram.new(quote_tier: quote_tier, fees_amount: fees_amount, program_type: program_type, group_code: group_code, effective_end_date: effective_end_date, effective_start_date: effective_start_date, account_id: @account.id)
       @account_program.save
     end
 
