9bdb6c403dd925b0397ebcc86b0a8ce35bad2fd3
Jason Amerine <amerine.19@gmail.com>
2018-02-25 11:24:14 -0500

    manual ig percent and admin screens

---
+++ app/admin/policy_calculations.rb
@@ -0,0 +1,48 @@
+ActiveAdmin.register PolicyCalculation do
+  actions :all, except: [:update, :destroy, :edit, :create, :new]
+  config.per_page = [25, 50, 100]
+
+  index do
+    selectable_column
+    column :representative do |i|
+      i.representative.abbreviated_name
+    end
+    column :policy_number
+    column :business_name
+    column '4yr Payroll', :policy_total_four_year_payroll
+    column 'Cred Group', :policy_credibility_group
+    column 'Max Claim Val', :policy_maximum_claim_value
+    column 'Cred %', :policy_credibility_percent
+    column 'Exp Loss', :policy_total_expected_losses
+    column 'TLL', :policy_total_limited_losses
+    column 'Claim Count', :policy_total_claims_count
+    column 'Group TML', :policy_total_modified_losses_group_reduced
+    column 'Ind TML', :policy_total_modified_losses_individual_reduced
+    column 'Group Ratio', :policy_group_ratio
+    column 'Ind Total Mod', :policy_individual_total_modifier
+    column 'Ind Exp Mod Rate', :policy_individual_experience_modified_rate
+    column 'IG', :policy_industry_group
+    column 'Current Payroll', :policy_total_current_payroll
+    column 'Std Premium', :policy_total_standard_premium
+    column 'Ind Premium', :policy_total_individual_premium
+    column 'Coverage Status', :current_coverage_status
+    column 'Status Eff Date', :coverage_status_effective_date
+    column 'Creation Date', :policy_creation_date
+    column 'FIN', :federal_identification_number
+    column 'CLM Rep', :currently_assigned_clm_representative_number
+    column 'Risk Rep', :currently_assigned_risk_representative_number
+    column 'ERC Rep', :currently_assigned_erc_representative_number
+    column 'GRC Rep', :currently_assigned_grc_representative_number
+    column 'Imm Successor', :immediate_successor_policy_number
+    column 'Ult Successor', :ultimate_successor_policy_number
+    column 'Coverage Type', :policy_coverage_type
+    column 'Employer Type', :policy_employer_type
+
+    actions
+  end
+
+  filter :representative, as: :select, collection: Representative.options_for_select
+  filter :policy_number, as: :numeric
+  filter :current_coverage_status
+
+end
--- app/models/account_program.rb
+++ app/models/account_program.rb
@@ -1,5 +1,6 @@
 class AccountProgram < ActiveRecord::Base
   belongs_to :account
+  has_one :representative, through: :account
 
   enum program_type: [:group_rating, :group_retro, :retainer, :grow_ohio, :one_claim_program, :em_cap]
 
@@ -11,5 +12,5 @@ class AccountProgram < ActiveRecord::Base
     obj.save
     obj
   end
-  
+
 end
