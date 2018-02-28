50afb2582f4c8713cd63ea2ef864b204d1e55eac
Jason Amerine <amerine.19@gmail.com>
2018-02-27 19:40:44 -0500

    changes to worker

--- app/workers/account_program_import.rb
+++ app/workers/account_program_import.rb
@@ -10,12 +10,12 @@ class AccountProgramImport
     if @representative
       @account = Account.find_by(representative_id: @representative.id, policy_number_entered: policy_number )
         if @account
-          fees_amount = hash["fees_amount"].gsub(/[^\d^\.]/, '').to_f
-          program_type = hash["program_type"].to_s
-          group_code = hash["group_code"].to_s
-          quote_tier = hash["quote_tier"].to_f
-          effective_start_date = hash["effective_start_date"].to_date
-          effective_end_date = hash["effective_end_date"].to_date
+          fees_amount = hash["fees_amount"]&.gsub(/[^\d^\.]/, '')&.to_f
+          program_type = hash["program_type"]&.to_s
+          group_code = hash["group_code"]&.to_s
+          quote_tier = hash["quote_tier"]&.to_f
+          effective_start_date = hash["effective_start_date"]&.to_date
+          effective_end_date = hash["effective_end_date"]&.to_date
           @account_program = AccountProgram.new(quote_tier: quote_tier, fees_amount: fees_amount, program_type: program_type, group_code: group_code, effective_end_date: effective_end_date, effective_start_date: effective_start_date, account_id: @account.id)
           @account_program.save
         end
