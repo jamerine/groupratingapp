35a83a2fede9c2623cb6529cf37343243b387a24
Jason Amerine <amerine.19@gmail.com>
2018-09-26 19:10:17 -0400

    group rating automation

--- app/services/group_rating_calculation.rb
+++ app/services/group_rating_calculation.rb
@@ -1,12 +1,15 @@
 class GroupRatingCalculation
-  def initialize account
+  def initialize account, options={}
     @account = account
+    @policy_calculation = account.policy_calculation
+    @manual_class_calculations = @policy_calculation.manual_class_calculations
   end
 
   def calculate
-    @account.policy_calculation.calculate_experience
-    @account.policy_calculation.calculate_premium
+    @policy_calculation.calculate_experience
+    @policy_calculation.calculate_premium
     @account.group_rating
     @account.group_retro
   end
+
 end
