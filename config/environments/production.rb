855ac38369b2b0bfa74b5de2b53d1f4d6461b3e6
Jason Amerine <amerine.19@gmail.com>
2019-01-23 20:34:34 -0500

    added SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'http://www.groupratingapp.herokuapp.com' }
-  # 
+  #
   # config.action_mailer.smtp_settings = { port: 25 }
   #
   # config.action_mailer.delivery_method = :smtp
