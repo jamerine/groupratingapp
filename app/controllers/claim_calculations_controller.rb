082f877e75c94eb2375785d765fb5eaad46e5b22
Newman <newman@Switchboxs-MacBook-Pro-2.local>
2019-11-27 16:10:05 -0500

    claim calculation show page first draft, waiting on feedback and MIRA2 mapping

--- app/controllers/claim_calculations_controller.rb
+++ app/controllers/claim_calculations_controller.rb
@@ -5,8 +5,7 @@ class ClaimCalculationsController < ApplicationController
 
   def show
     @claim_calculation = ClaimCalculation.find_by(id: params[:id])
+    @account = Account.find(@claim_calculation.policy_calculation.account_id)
     @group_rating = GroupRating.where(process_representative: @claim_calculation.representative_number).last
   end
-
-
 end
