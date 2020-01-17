c359d0349827b6c3be0e045506e326c02dfc5729
Adon Moyer <amoyer@switchboxinc.com>
2020-01-17 13:45:29 -0500

    Fix issue where dropdown would stay open

--- app/controllers/policy_calculations_controller.rb
+++ app/controllers/policy_calculations_controller.rb
@@ -1,5 +1,4 @@
 class PolicyCalculationsController < ApplicationController
-  before_action :policy_search_results, only: %w[search]
 
   def index
     @policy_calculations = PolicyCalculation.all
@@ -38,36 +37,13 @@ class PolicyCalculationsController < ApplicationController
   end
 
   def search
-    render json: { matchingPoliciesList: @policy_search_results }
+    @search_value          = params[:search_value]
+    @policy_search_results = PolicyCalculation.joins(:account).where("policy_calculations.policy_number::text ILIKE :search_value OR REPLACE(policy_calculations.policy_number::text, '-', '') ILIKE :search_value OR accounts.name ILIKE :search_value", search_value: "%#{@search_value}%").uniq
+
+    render json: { matchingPoliciesList: render_to_string('policy_calculations/_search-results', layout: false) }
   end
 
   private
-
-  def search_value
-    params[:search_value]
-  end
-
-  def policy_search_results
-    policy_search_results = matching_policy_calculations.each.with_object([]) do |matching_policy_calculation, matching_li_collection|
-      matching_li_collection << "<a target='_blank' href='#{policy_calculation_path(matching_policy_calculation)}'><li><div class='policy-number'>#{matching_policy_calculation.policy_number}</div><span class='account-name'>#{matching_policy_calculation.account_name&.strip}</span></li></a>"
-    end
-
-    @policy_search_results = policy_search_results.length.zero? ? no_matching_results_for_search_value : policy_search_results.join('')
-  end
-
-  def matching_policy_calculations
-    PolicyCalculation.joins(:account).where("policy_calculations.policy_number::text ILIKE :search_value OR REPLACE(policy_calculations.policy_number::text, '-', '') ILIKE :search_value OR accounts.name ILIKE :search_value", search_value: "%#{search_value}%").uniq
-  end
-
-  def no_matching_results_for_search_value
-    "<li style='text-align: center;'>No matching results for: <b>#{search_value}</b></li>"
-  end
-
-  def does_result_value_ends_with_search_value(result_value)
-    result_value_length = result_value.length
-
-    result_value[(result_value_length - search_value.length)..(result_value_length - 1)] == search_value
-  end
 
   def policy_calculation_params
     params.require(:policy_calculation).permit(:representative_number, :policy_number, :policy_group_number, :policy_status, :policy_total_four_year_payroll, :policy_credibility_group, :policy_maximum_claim_value, :policy_credibility_percent, :policy_total_expected_losses, :policy_total_limited_losses, :policy_total_claims_count, :policy_total_modified_losses_group_reduced, :policy_total_modified_losses_individual_reduced, :policy_group_ratio, :policy_individual_total_modifier, :policy_individual_experience_modified_rate, :group_rating_qualification, :group_rating_tier, :group_rating_group_number, :policy_total_current_payroll, :policy_total_standard_premium, :policy_total_individual_premium, :policy_total_group_premium, :policy_total_group_savings, :policy_group_fees, :policy_group_dues, :policy_total_costs, :data_source, :representative_id)
