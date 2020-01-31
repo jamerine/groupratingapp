6132a3b0e7769393c97917dd85ba99e3fa541d56
Sean Temple <stemple@switchboxinc.com>
2020-01-31 07:56:07 -0500

    Add Digial Ocean deployment

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.2"
+
+set :application, "groupratingapp"
+set :repo_url, "git@bitbucket.org:switchbox/arm-group-rating.git"
+
+# Default branch is :master
+set :branch, ENV['branch'] || 'master'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", "config/environments/production.rb"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
+
+# Default value for default_env is {}
+set :default_env, { path: "/home/deploy/.gem/ruby/2.3.0/bin:$PATH" }
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+set :slack_url, 'https://hooks.slack.com/services/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'
+set :slack_channel, ['#deployments']
+set :slack_username, 'groupratingapp'
