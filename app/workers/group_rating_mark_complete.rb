3133c4576e3b8998620d9cb4b273b9622505d2fa
Adon Moyer <amoyer@switchboxinc.com>
2020-01-31 11:05:18 -0500

    Process updates

--- app/workers/group_rating_mark_complete.rb
+++ app/workers/group_rating_mark_complete.rb
@@ -5,7 +5,7 @@ class GroupRatingMarkComplete
   sidekiq_options queue: :group_rating_mark_complete, retry: 3
 
   def perform(group_rating_id, all_process = nil)
-    @representatives_count = Representative.last.id
+    @representatives_count = Representative.last&.id
     @group_rating          = GroupRating.find_by(id: group_rating_id)
     @group_rating.status   = "Completed"
     @group_rating.save
@@ -16,9 +16,9 @@ class GroupRatingMarkComplete
         while i <= @representatives_count
           i               += 1
           @representative = Representative.find_by(id: i)
-          break if !@representative.nil?
+          break unless @representative.nil?
         end
-        GroupRating.where(representative_id: (@representative.id)).destroy_all
+        GroupRating.where(representative_id: (@representative&.id)).destroy_all
         @new_group_rating                        = GroupRating.new(experience_period_lower_date: @representative.experience_period_lower_date, experience_period_upper_date: @representative.experience_period_upper_date, current_payroll_period_lower_date: @representative.current_payroll_period_lower_date, current_payroll_period_upper_date: @representative.current_payroll_period_upper_date, current_payroll_year: @representative.current_payroll_year, program_year_lower_date: @representative.program_year_lower_date, program_year_upper_date: @representative.program_year_upper_date, program_year: @representative.program_year, quote_year_lower_date: @representative.quote_year_lower_date, quote_year_upper_date: @representative.quote_year_upper_date, quote_year: @representative.quote_year, representative_id: @representative.id)
         @new_group_rating.process_representative = @representative.representative_number
         @new_group_rating.status                 = 'Queuing'
