769ba9f353206c5a532d17ab32caf924cf719d79
Adon Moyer <amoyer@switchboxinc.com>
2020-02-03 10:17:38 -0500

    Clean up to mark complete process

--- app/workers/handle_manual_policy_calculations_process.rb
+++ app/workers/handle_manual_policy_calculations_process.rb
@@ -2,11 +2,11 @@ class HandleManualPolicyCalculationsProcess
   require 'sidekiq/api'
   include Sidekiq::Worker
 
-  sidekiq_options queue: :handle_manual_policy_calculations_process, retry: 1
+  sidekiq_options queue: :handle_manual_policy_calculations_process, retry: 3
 
   def perform(calculation_id)
-    return if calculation_id.nil?
+    return unless calculation_id.present?
 
-    PayrollCalculation.find(calculation_id)&.update_attribute(:manual_class_payroll, 0.00)
+    PayrollCalculation.find_by(id: calculation_id)&.update_attribute(:manual_class_payroll, 0.00)
   end
 end
