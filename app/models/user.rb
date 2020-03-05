d695f2a5e353b90d4bf11aacaaf7faa296816309
Adon Moyer <amoyer@switchboxinc.com>
2020-03-05 11:25:07 -0500

    Start adding users views and other controller actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,13 +31,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :representatives_users
+  has_many :representatives_users, dependent: :destroy
   has_many :representatives, through: :representatives_users
   has_many :notes
 
-
   enum role: [:admin, :client, :general, :read_only]
-
 
   def full_name
     "#{first_name} #{last_name}"
