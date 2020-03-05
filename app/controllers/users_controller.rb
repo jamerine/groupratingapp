a13990da3435d5e6bdcd71f67e11d25c1ae376c0
Adon Moyer <amoyer@switchboxinc.com>
2020-03-05 13:42:04 -0500

    Finish up users and the correct controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,52 @@
 class UsersController < ApplicationController
   before_filter :authorize_admin, only: [:create, :index]
   before_action :set_user, only: [:edit, :update, :destroy]
+  before_action :handle_params, only: [:create, :update]
 
   def index
     @users = User.all.order(last_name: :asc, first_name: :asc)
   end
 
   def new
-    @representative = Representative.find_by(id: params[:representative_id]) || Representative.first
+    @representative = Representative.find_by(id: params[:representative_id])
     @user           = User.new
-    @roles          = User.roles
+
+    @user.representatives << @representative if @representative.present?
   end
 
   def create
-    @representative = Representative.find(params[:representative_id])
-    @user           = User.new(user_params)
+    @user = User.new(@user_params)
 
     if @user.save
-      @representatives_user = RepresentativesUser.create(user_id: @user.id, representative_id: @representative.id)
-      redirect_to @representative, flash: { notice: "User Created" }
+      @user_params[:representative_ids].each do |rep_id|
+        RepresentativesUser.find_or_create_by(user_id: @user.id, representative_id: rep_id)
+      end
+
+      flash[:success] = 'User Created!'
+      redirect_to action: :index
     else
-      redirect_to @representative, flash: { alert: "Error creating a user" }
+      flash[:danger] = "Error Creating User!"
+      render :edit
     end
   end
 
   def edit
-
   end
 
   def update
+    @user.assign_attributes(@user_params)
 
+    if @user.save
+      @user_params[:representative_ids].each do |rep_id|
+        RepresentativesUser.find_or_create_by(user_id: @user.id, representative_id: rep_id)
+      end
+
+      flash[:success] = 'User Updated!'
+      redirect_to action: :index
+    else
+      flash[:danger] = 'Something went wrong, please try again.'
+      render :edit
+    end
   end
 
   def destroy
@@ -43,7 +60,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:role, :first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:role, :first_name, :last_name, :email, :password, :password_confirmation, representative_ids: [])
   end
 
   def set_user
@@ -55,5 +72,15 @@ class UsersController < ApplicationController
   def authorize_admin
     return if current_user.admin?
     redirect_to root_path, flash: { alert: 'Admins only!' }
+  end
+
+  def handle_params
+    @user_params        = user_params
+    @user_params[:role] = @user_params[:role]&.to_i unless @user_params[:role].blank?
+
+    return @user_params unless @user.present?
+
+    @user_params.delete(:password) if user_params[:password].blank? && @user.encrypted_password.present?
+    @user_params.delete(:password_confirmation) if user_params[:password].blank? && @user.encrypted_password.present?
   end
 end
