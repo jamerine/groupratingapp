1a999bd9edc4a09b64cc5d83098c7bba0c48ff9d
Adon Moyer <amoyer@switchboxinc.com>
2020-03-06 09:45:52 -0500

    Environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,4 +19,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors   = true
   config.action_mailer.perform_deliveries      = true
   config.action_controller.include_all_helpers = true
+
+  Rails.application.default_url_options[:host] = 'localhost:3000'
 end
