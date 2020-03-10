3711fc7a27252f9d64d6ee60a4f59d91131f162d
Adon Moyer <amoyer@switchboxinc.com>
2020-03-10 16:01:38 -0400

    Bugfix notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,29 +6,32 @@ class NotesController < ApplicationController
   end
 
   def new
-    @account = Account.find(params[:account_id])
-    @note = current_user.notes.build
+    @account    = Account.find(params[:account_id])
+    @note       = current_user.notes.build
     @categories = Note.categories
   end
 
   def create
-    @account = Account.find(params[:account_id])
-    @note = @account.notes.build(note_params)
-    @note.assign_attributes(user_id: current_user.id)
+    @account         = Account.find(params[:account_id])
+    @categories      = Note.categories
+    @note            = Note.new(note_params)
+    @note.account_id = @account.id
+    @note.user_id    = current_user.id
+
     if @note.save
       flash[:notice] = "Notes was created successfully"
       redirect_to account_path(@account)
     else
       flash[:alert] = "There was an error creating note. Please try again."
-      render 'new'
+      render :new
     end
   end
 
   def index
-    @account = Account.find(params[:account_id])
-    @notes = @account.notes.order(created_at: :desc)
+    @account    = Account.find(params[:account_id])
+    @notes      = @account.notes.order(created_at: :desc)
     @categories = Note.categories
-    @users = []
+    @users      = []
     @notes.each do |note|
       unless @users.include? note.user
         @users << note.user
@@ -44,13 +47,14 @@ class NotesController < ApplicationController
   end
 
   def update
+    @categories = Note.categories
     @note.assign_attributes(note_params)
     if @note.save
       flash[:notice] = "Notes was updated successfully"
       redirect_to account_note_path(@account, @note)
     else
       flash[:alert] = "There was an error updating note. Please try again."
-      redirect_to account_note_path(@account, @note)
+      render :edit
     end
   end
 
@@ -79,9 +83,8 @@ class NotesController < ApplicationController
 
   def find_note
     @account = Account.find(params[:account_id])
-    @note = @account.notes.find(params[:id])
+    @note    = @account.notes.find(params[:id])
   end
-
 
   def note_params
     params.require(:note).permit(:category, :description, :title, :attachment)
