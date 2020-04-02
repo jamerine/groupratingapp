32a336cce125a98031e59db1ec4f3df2006a06f4
Adon Moyer <amoyer@switchboxinc.com>
2020-04-01 23:16:06 -0400

    Make sure that sidekiq has concurrency

--- lib/sidekiq_calculations.rb
+++ lib/sidekiq_calculations.rb
@@ -19,48 +19,47 @@ Sidekiq Server Configuration failed.
   end
 
   def client_redis_size
-    return DEFAULT_CLIENT_REDIS_SIZE if !Rails.env.production?
-
-    puma_workers * (puma_threads/2) * web_dynos
+    Rails.env.production? ? 4 : DEFAULT_CLIENT_REDIS_SIZE
+      # puma_workers * (puma_threads/2) * web_dynos
   end
 
   def server_concurrency_size
-    return DEFAULT_SERVER_CONCURRENCY if !Rails.env.production?
-
-    (max_redis_connection - client_redis_size - sidekiq_reserved) / worker_dynos / paranoid_divisor
+    Rails.env.production? ? ENV['WEB_CONCURRENCY'] : DEFAULT_SERVER_CONCURRENCY
+    # (max_redis_connection - client_redis_size - sidekiq_reserved) / worker_dynos / paranoid_divisor
   end
 
   private
-    def web_dynos
-      Integer(ENV['NUMBER_OF_WEB_DYNOS'])
-    end
 
-    def worker_dynos
-      Integer(ENV['NUMBER_OF_WORKER_DYNOS'])
-    end
+  def web_dynos
+    Integer(ENV['NUMBER_OF_WEB_DYNOS'])
+  end
 
-    def max_redis_connection
-      Integer(ENV['MAX_REDIS_CONNECTION'])
-    end
+  def worker_dynos
+    Integer(ENV['NUMBER_OF_WORKER_DYNOS'])
+  end
 
-    # ENV used in `config/puma.rb` too.
-    def puma_workers
-      Integer(ENV.fetch("WEB_CONCURRENCY", 2))
-    end
+  def max_redis_connection
+    Integer(ENV['MAX_REDIS_CONNECTION'])
+  end
 
-    # ENV used in `config/puma.rb` too.
-    def puma_threads
-      Integer(ENV.fetch("WEB_MAX_THREADS", 5))
-    end
+  # ENV used in `config/puma.rb` too.
+  def puma_workers
+    Integer(ENV.fetch("WEB_CONCURRENCY", 2))
+  end
 
-    # https://github.com/mperham/sidekiq/blob/master/lib/sidekiq/redis_connection.rb#L12
-    def sidekiq_reserved
-      5
-    end
+  # ENV used in `config/puma.rb` too.
+  def puma_threads
+    Integer(ENV.fetch("WEB_MAX_THREADS", 5))
+  end
 
-    # This is added to bring down the value of Concurrency
-    # so that there's leeway to grow
-    def paranoid_divisor
-      2
-    end
+  # https://github.com/mperham/sidekiq/blob/master/lib/sidekiq/redis_connection.rb#L12
+  def sidekiq_reserved
+    5
+  end
+
+  # This is added to bring down the value of Concurrency
+  # so that there's leeway to grow
+  def paranoid_divisor
+    2
+  end
 end
