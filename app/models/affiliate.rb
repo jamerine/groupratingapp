a75259a70193472a3867711b58a306ba0498c3a8
Adon Moyer <amoyer@switchboxinc.com>
2020-05-13 09:13:47 -0400

    Adds roles

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -32,7 +32,7 @@ class Affiliate < ActiveRecord::Base
   # before_save {self.last_name = last_name.downcase.titleize}
   # before_save {self.email_address = email_address.downcase}
 
-  enum role: [:accountant, :association, :company_code, :crm, :examiner, :member, :sales, :referral]
+  enum role: [:accountant, :association, :company_code, :crm, :examiner, :account_examiner, :member, :sales, :referral]
   enum internal_external: [:external, :internal]
 
   private
