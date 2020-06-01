b628db89464f8c1d37bc43ed98828f697d180c49
Adon Moyer <amoyer@switchboxinc.com>
2020-06-01 11:36:39 -0400

    Encoding for ANSI

--- app/workers/contact_import.rb
+++ app/workers/contact_import.rb
@@ -4,7 +4,6 @@ class ContactImport
   sidekiq_options queue: :import_file, retry: 1
 
   def perform(contact_hash)
-
     @representative = Representative.find_by(representative_number: contact_hash["representative_number"])
     @account = Account.find_by(representative_id: @representative.id, policy_number_entered: contact_hash["policy_number"] )
 
@@ -12,6 +11,5 @@ class ContactImport
       @contact = Contact.create!(contact_hash.except("representative_number", "policy_number"))
       AccountsContact.create!(contact_id: @contact.id, account_id: @account.id)
     end
-
   end
 end
