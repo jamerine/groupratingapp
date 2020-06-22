7e62b1208a15b30d4200f63c83665ef7395f412b
Adon Moyer <amoyer@switchboxinc.com>
2020-06-22 19:58:26 -0400

    Put in body to update

--- app/workers/import_mira_files_process.rb
+++ app/workers/import_mira_files_process.rb
@@ -13,7 +13,6 @@ class ImportMiraFilesProcess
       record = MiraDetailRecord.find_or_create_by({ representative_number:    mira.representative_number,
                                                     record_type:              mira.record_type,
                                                     requestor_number:         mira.requestor_number,
-                                                    claim_number:             mira.claim_number,
                                                     policy_number:            mira.policy_number,
                                                     business_sequence_number: mira.business_sequence_number
                                                   })
@@ -23,7 +22,6 @@ class ImportMiraFilesProcess
     WeeklyMira.by_representative(representative_number).by_record_type.find_each do |mira|
       record = WeeklyMiraDetailRecord.find_or_create_by({ representative_number:    mira.representative_number,
                                                           record_type:              mira.record_type,
-                                                          claim_number:             mira.claim_number,
                                                           requestor_number:         mira.requestor_number,
                                                           policy_number:            mira.policy_number,
                                                           business_sequence_number: mira.business_sequence_number
@@ -75,6 +73,7 @@ class ImportMiraFilesProcess
   def gather_attributes(mira)
     { valid_policy_number:                                    mira.valid_policy_number,
       claim_indicator:                                        mira.claim_indicator,
+      claim_number:                                           mira.claim_number,
       claim_injury_date:                                      mira.claim_injury_date,
       claim_filing_date:                                      mira.claim_filing_date,
       claim_hire_date:                                        mira.claim_hire_date,
