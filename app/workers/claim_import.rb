6b2ff5d33f7c0ed7e5c09ee81b8c4fbf7d9b49a2
Adon Moyer <amoyer@switchboxinc.com>
2020-08-12 14:01:30 -0400

    Comment the claim experience calculation

--- app/workers/claim_import.rb
+++ app/workers/claim_import.rb
@@ -14,10 +14,10 @@ class ClaimImport
       if policy_calculation
         @claim_calculation = ClaimCalculation.new(representative_number: representative.representative_number, policy_number: policy_calculation.policy_number, policy_calculation_id: policy_calculation.id, data_source: 'user' )
         @claim_calculation.assign_attributes(claim_hash.except("representative_number", "policy_number"))
-        claim_unlimimited_limited_loss = (@claim_calculation.claim_medical_paid + @claim_calculation.claim_mira_medical_reserve_amount + @claim_calculation.claim_mira_non_reducible_indemnity_paid + @claim_calculation.claim_mira_reducible_indemnity_paid + @claim_calculation.claim_mira_indemnity_reserve_amount + @claim_calculation.claim_mira_non_reducible_indemnity_paid_2)
-        @claim_calculation.assign_attributes(claim_unlimited_limited_loss: claim_unlimimited_limited_loss)
+        # claim_unlimited_limited_loss = (@claim_calculation.claim_medical_paid + @claim_calculation.claim_mira_medical_reserve_amount + @claim_calculation.claim_mira_non_reducible_indemnity_paid + @claim_calculation.claim_mira_reducible_indemnity_paid + @claim_calculation.claim_mira_indemnity_reserve_amount + @claim_calculation.claim_mira_non_reducible_indemnity_paid_2)
+        # @claim_calculation.assign_attributes(claim_unlimited_limited_loss: claim_unlimited_limited_loss)
         @claim_calculation.save!
-        @claim_calculation.recalculate_experience(policy_calculation.policy_maximum_claim_value)
+        # @claim_calculation.recalculate_experience(policy_calculation.policy_maximum_claim_value)
       end
 
     end
