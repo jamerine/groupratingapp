0616e5ed54ab97bf101f502808202a3e903f63c5
Adon Moyer <amoyer@switchboxinc.com>
2020-08-18 07:41:20 -0400

    Don't create the unique index

--- db/migrate/20200818113352_remove_constraint_for_clicds.rb
+++
@@ -1,8 +0,0 @@
-class RemoveConstraintForClicds < ActiveRecord::Migration
-  def change
-    execute <<-SQL
-      ALTER TABLE clicd_detail_records
-      DROP CONSTRAINT unique_records_for_clicds;
-    SQL
-  end
-end
