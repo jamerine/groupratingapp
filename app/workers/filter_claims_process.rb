4faafb8ddc5f13db712cbe6cb9a5c9d5e6145a17
Adon Moyer <amoyer@switchboxinc.com>
2020-08-19 19:38:45 -0400

    Handling deleting claims per client request

--- app/workers/filter_claims_process.rb
+++ app/workers/filter_claims_process.rb
@@ -10,21 +10,23 @@ class FilterClaimsProcess
 
     @representative = Representative.find_by(id: @group_rating.representative_id)
 
-    result = ActiveRecord::Base.connection.exec_query("SELECT DISTINCT claims.id
-                                                    FROM claim_calculations claims
-                                                    LEFT JOIN policy_calculations policies
-                                                    ON policies.policy_number = claims.policy_number
-                                                    WHERE claims.representative_number = #{@representative.representative_number}
-                                                    AND claims.claim_injury_date >= '#{BwcCodesConstantValue.current_rate.start_date - 10.years}'
-                                                    AND policies.current_coverage_status IN ('ACTIV', 'REINS', 'LAPSE')
-                                                    AND NOT EXISTS (SELECT 1
+    result = ActiveRecord::Base.connection.exec_query("DELETE FROM claim_calculations claims
+                                                      LEFT JOIN policy_calculations policies
+                                                      ON 	policies.policy_number = claims.policy_number AND policies.representative_number = #{@representative.representative_number}
+                                                      WHERE claims.representative_number = #{@representative.representative_number}
+                                                      AND policies.current_coverage_status IN ('ACTIV', 'REINS', 'LAPSE')
+                                                      AND claims.claim_injury_date >= '#{BwcCodesConstantValue.current_rate.start_date - 10.years}'
+                                                      AND claims.data_source = 'bwc'
+                                                      AND NOT EXISTS (SELECT 1
                                                                     FROM democ_detail_records democs
-                                                                    WHERE democs.claim_number = claims.claim_number)
-                                                    AND EXISTS (SELECT 1
-                                                                FROM democ_detail_records democs
-                                                                WHERE democs.policy_number = claims.policy_number)")
-
-    claim_ids = result.rows.flatten.map(&:to_i)
+                                                                    WHERE democs.claim_number = claims.claim_number
+                                                                      AND democs.policy_number = claims.policy_number
+                                                                      AND democs.representative_number = #{@representative.representative_number})
+                                                      AND EXISTS (SELECT 1
+                                                                  FROM democ_detail_records democs
+                                                                  WHERE democs.policy_number = policies.policy_number
+                                                                    AND democs.representative_number = #{@representative.representative_number})")
+    result.clear
 
     GroupRatingMarkComplete.perform_async(group_rating_id, all_process)
   end
