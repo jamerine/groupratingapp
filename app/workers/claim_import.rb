f8d51538d640056b835746d5278c1077b4ebe9a9
Adon Moyer <amoyer@switchboxinc.com>
2020-08-19 22:30:25 -0400

    Claim import update

--- app/workers/claim_import.rb
+++ app/workers/claim_import.rb
@@ -12,8 +12,7 @@ class ClaimImport
       policy_calculation = PolicyCalculation.find_by(account_id: account.id)
 
       if policy_calculation
-        @claim_calculation = ClaimCalculation.new(representative_number: representative.representative_number, policy_number: policy_calculation.policy_number, policy_calculation_id: policy_calculation.id, data_source: 'user' )
-        @claim_calculation.assign_attributes(claim_hash.except("representative_number", "policy_number"))
+        @claim_calculation = ClaimCalculation.where(representative_number: representative.representative_number, policy_number: policy_calculation.policy_number, policy_calculation_id: policy_calculation.id, data_source: 'user' ).update_or_create(claim_hash.except("representative_number", "policy_number"))
         # claim_unlimited_limited_loss = (@claim_calculation.claim_medical_paid + @claim_calculation.claim_mira_medical_reserve_amount + @claim_calculation.claim_mira_non_reducible_indemnity_paid + @claim_calculation.claim_mira_reducible_indemnity_paid + @claim_calculation.claim_mira_indemnity_reserve_amount + @claim_calculation.claim_mira_non_reducible_indemnity_paid_2)
         # @claim_calculation.assign_attributes(claim_unlimited_limited_loss: claim_unlimited_limited_loss)
         @claim_calculation.save!
