2a78d85a5c5d39827ee603671d007c391dd64563
Adon Moyer <amoyer@switchboxinc.com>
2020-08-19 22:32:41 -0400

    Claim number in import update

--- app/workers/claim_import.rb
+++ app/workers/claim_import.rb
@@ -6,13 +6,13 @@ class ClaimImport
   def perform(claim_hash)
 
     representative = Representative.find_by(representative_number: claim_hash["representative_number"])
-    account = Account.find_by(representative_id: representative.id, policy_number_entered: claim_hash["policy_number"] )
+    account        = Account.find_by(representative_id: representative.id, policy_number_entered: claim_hash["policy_number"])
 
     if account
       policy_calculation = PolicyCalculation.find_by(account_id: account.id)
 
       if policy_calculation
-        @claim_calculation = ClaimCalculation.where(representative_number: representative.representative_number, policy_number: policy_calculation.policy_number, policy_calculation_id: policy_calculation.id, data_source: 'user' ).update_or_create(claim_hash.except("representative_number", "policy_number"))
+        @claim_calculation = ClaimCalculation.where(representative_number: representative.representative_number, policy_number: policy_calculation.policy_number, policy_calculation_id: policy_calculation.id, data_source: 'user', claim_number: "#{claim_hash['claim_number'] }").update_or_create(claim_hash.except("representative_number", "policy_number", "claim_number"))
         # claim_unlimited_limited_loss = (@claim_calculation.claim_medical_paid + @claim_calculation.claim_mira_medical_reserve_amount + @claim_calculation.claim_mira_non_reducible_indemnity_paid + @claim_calculation.claim_mira_reducible_indemnity_paid + @claim_calculation.claim_mira_indemnity_reserve_amount + @claim_calculation.claim_mira_non_reducible_indemnity_paid_2)
         # @claim_calculation.assign_attributes(claim_unlimited_limited_loss: claim_unlimited_limited_loss)
         @claim_calculation.save!
