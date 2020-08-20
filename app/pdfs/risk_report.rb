ce07bd46ce5803639c59a37b828c7f887447229e
Adon Moyer <amoyer@switchboxinc.com>
2020-08-20 10:48:56 -0400

    Typo

--- app/pdfs/risk_report.rb
+++ app/pdfs/risk_report.rb
@@ -1102,7 +1102,7 @@ class RiskReport < PdfReport
         mira_res     = "#{round((1 - e.claim_handicap_percent) * (e.claim_mira_medical_reserve_amount + (e.claim_mira_indemnity_reserve_amount)) * e.claim_group_multiplier * (1 - e.claim_subrogation_percent), 0)}"
       end
 
-      [e.claim_number, e.claimant_name.titleize, e.claim_injury_date.in_time_zone("America/New_York").strftime("%m/%d/%y"), e.claim_manual_number, comp_awarded, medical_paid, mira_res, round(e.claim_modified_losses_group_reduced, 0), round(e.claim_modified_losses_individual_reduced, 0), "#{round((e.claim_unlimited_limited_loss || 0) - (e.claim_total_subrogation_collected || 0), 0)}", percent(e.claim_handicap_percent), "#{claim_code_calc(e)}"] }
+      [e.claim_number, e.claimant_name.titleize, e.claim_injury_date.in_time_zone("America/New_York").strftime("%m/%d/%y"), e.claim_manual_number, comp_awarded, medical_paid, mira_res, round(e.claim_modified_losses_group_reduced, 0), round(e.claim_modified_losses_individual_reduced, 0), "#{round((e.claim_unlimited_limited_loss || 0) - (e.claim_total_subrogation_collected || 0), 0)}", percent(e.claim_handicap_percent), "#{claim_code_calc(e)}"]
     end
     @data += [[{ :content => "Totals", :colspan => 4 }, "#{round(comp_total, 0)}", "#{round(med_paid_total, 0)}", "#{round(mira_res_total, 0)}", "#{round(claims_array.sum(:claim_modified_losses_group_reduced), 0)}", "#{round(claims_array.sum(:claim_modified_losses_individual_reduced), 0)}", "#{round(claims_array.sum(:claim_unlimited_limited_loss) - claims_array.sum(:claim_total_subrogation_collected), 0)}", "", ""]]
   end
