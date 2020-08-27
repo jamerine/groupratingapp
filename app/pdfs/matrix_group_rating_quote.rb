67ea6e33f9d7cca92da666d9b74fda4e69106b46
Adon Moyer <amoyer@switchboxinc.com>
2020-08-26 20:51:17 -0400

    Dont take out fees in matrix group rating quotes

--- app/pdfs/matrix_group_rating_quote.rb
+++ app/pdfs/matrix_group_rating_quote.rb
@@ -90,6 +90,6 @@ class MatrixGroupRatingQuote < MatrixPdfReport
     @data = [["<color rgb='FFFFFF'>#{@account.representative.quote_year} ESTIMATED SAVINGS</color>", "<color rgb='FFFFFF'>#{percent(@account.group_rating_tier)} GROUP</color>"]]
     @data += [["Individual Premium", price(@policy_calculation.policy_total_individual_premium)]]
     @data += [["Net Premium", price(@account.group_premium)]]
-    @data += [["Total Savings", price(@account.group_savings - @group_fees)]]
+    @data += [["Total Savings", price(@account.group_savings)]]
   end
 end
