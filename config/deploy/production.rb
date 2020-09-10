31a6357755089b776dbc39407fc3eef6c64aa36b
Adon Moyer <amoyer@switchboxinc.com>
2020-09-10 09:23:56 -0400

    Concurrency and error notify

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server "64.225.21.47", user: "deploy", roles: %w{app db web}
-set :sidekiq_processes, 2
+set :sidekiq_processes, 3
 # set :slack_url, ENV['WEBHOOK_URL']
 # set :slack_channel, ['#arm-errors']
 # set :slack_username, 'groupratingapp'
--- lib/sidekiq_error_notifier.rb
+++ lib/sidekiq_error_notifier.rb
@@ -1,6 +1,5 @@
 class SidekiqErrorNotifier
-
   def self.notify(exception, context_hash)
-    ErrorMailer.notify_about_sidekiq_exception(exception, context_hash).deliver_later
+    # ErrorMailer.notify_about_sidekiq_exception(exception, context_hash).deliver_later
   end
 end
