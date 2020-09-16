135b7666d69e842751e17a894380c1a81ecfc3ab
Adon Moyer <amoyer@switchboxinc.com>
2020-09-16 16:19:25 -0400

    Code alignment

--- app/controllers/manual_class_calculations_controller.rb
+++ app/controllers/manual_class_calculations_controller.rb
@@ -5,14 +5,13 @@ class ManualClassCalculationsController < ApplicationController
 
   def show
     @manual_class_calculation = ManualClassCalculation.find_by(id: params[:id])
-    @group_rating = GroupRating.where(process_representative: @manual_class_calculation.representative_number).last
-    @policy_calculation = PolicyCalculation.find_by(id: @manual_class_calculation.policy_calculation_id)
-    @account = @policy_calculation.account
+    @group_rating             = GroupRating.where(process_representative: @manual_class_calculation.representative_number).last
+    @policy_calculation       = PolicyCalculation.find_by(id: @manual_class_calculation.policy_calculation_id)
+    @account                  = @policy_calculation.account
     @payroll_calculations     = PayrollCalculation.where(manual_class_calculation_id: @manual_class_calculation.id, representative_number: @manual_class_calculation.representative_number, policy_number: @manual_class_calculation.policy_number)
-    @new_payroll_calculation = PayrollCalculation.new
+    @new_payroll_calculation  = PayrollCalculation.new
   end
 
-
- private
+  private
 
 end
