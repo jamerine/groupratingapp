6dd982117c7f44ab1eb65a1dc836666d1c1d617b
Adon Moyer <amoyer@switchboxinc.com>
2020-10-12 15:48:21 -0400

    Need to add rest of files

--- app/helpers/import_helper.rb
+++ app/helpers/import_helper.rb
@@ -6,7 +6,13 @@ module ImportHelper
       [:democs, 'Democs'],
       [:rates, 'Rates'],
       [:pdemos, 'Policy Demographics'],
-      [:pcombs, 'Policy Combinations']
+      [:pcombs, 'Policy Combinations'],
+      [:mrcls, 'Manual Re-classes'],
+      [:mremps, 'Mremps'],
+      [:phmgns, 'Phmgns'],
+      [:sc230, 'SC230'],
+      [:pemhs, 'Pemhs'],
+      [:pcovgs, 'Policy Coverages']
     ]
   end
 end
