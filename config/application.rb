9fc0290c89a97262ce2940ba39d06d9974e86ca8
Adon Moyer <amoyer@switchboxinc.com>
2020-10-14 10:08:17 -0400

    Application.rb env file

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,11 @@ module Groupratingapp
     # To create schema file for postgres
     # config.active_record.schema_format = :sql
 
-
-
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'environment.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
