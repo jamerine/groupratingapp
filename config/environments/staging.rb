caa0803bfd64d34ff3569dd0c3bfa44bf83abffa
Adon Moyer <amoyer@switchboxinc.com>
2021-01-12 10:38:02 -0500

    Mail domain

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
     address:              'smtp.mailgun.org',
     user_name:            ENV['MAILGUN_USERNAME'],
     password:             ENV['MAILGUN_PASSWORD'],
-    domain:               'groupratingapp.staging.switchboxdev.com',
+    domain:               'groupratingapp.switchboxdinc.com',
     authentication:       'login',
     enable_starttls_auto: true
   }
