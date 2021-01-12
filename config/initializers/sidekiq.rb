7d24573bd4bb7fe39454bd519bcfb452cae83a27
Adon Moyer <amoyer@switchboxinc.com>
2021-01-12 13:32:51 -0500

    Staging sidekiq settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,8 +8,9 @@ Sidekiq.configure_client do |config|
   sidekiq_calculations.raise_error_for_env!
 
   config.redis = {
-    url:  ENV['REDISCLOUD_URL'],
-    size: sidekiq_calculations.client_redis_size
+    url:       ENV['REDISCLOUD_URL'],
+    size:      sidekiq_calculations.client_redis_size,
+    namespace: "groupratingapp_#{Rails.env}"
   }
 end
 
@@ -19,12 +20,13 @@ Sidekiq.configure_server do |config|
 
   config.options[:concurrency] = sidekiq_calculations.server_concurrency_size
   config.redis                 = {
-    url: ENV['REDISCLOUD_URL']
+    url:       ENV['REDISCLOUD_URL'],
+    namespace: "groupratingapp_#{Rails.env}"
   }
 
   config.logger.level = ::Logger::INFO
 
-  Rails.logger = Sidekiq::Logging.logger
+  Rails.logger              = Sidekiq::Logging.logger
   ActiveRecord::Base.logger = Sidekiq::Logging.logger
 
   config.error_handlers << Proc.new { |exception, context_hash| SidekiqErrorNotifier.notify(exception, context_hash) }
