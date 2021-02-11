c655682cb3c64d8f8b24ff554e8859fc8405a208
Adon Moyer <amoyer@switchboxinc.com>
2021-02-11 16:49:48 -0500

    New IP again

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # server "64.225.21.47", user: "deploy", roles: %w{app db web}
-server "159.89.240.32", user: "deploy", roles: %w{app db web}
+server "161.35.188.233", user: "deploy", roles: %w{app db web}
 set :sidekiq_processes, 3
 set :sidekiq_env, :production
 # Default value for :linked_files is []
