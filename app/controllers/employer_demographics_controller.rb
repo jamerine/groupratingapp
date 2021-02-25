d5466008f001f9a03b0e47e90b8b42c6f50e2b3b
Adon Moyer <amoyer@switchboxinc.com>
2021-02-25 11:08:34 -0500

    Checking Employer demographics index and saving policy details to imports on new account

--- app/controllers/employer_demographics_controller.rb
+++ app/controllers/employer_demographics_controller.rb
@@ -1,10 +1,11 @@
 class EmployerDemographicsController < ApplicationController
 
   def index
+    @field_names    = EmployerDemographic.column_names.reject { |col| col if col.to_sym.in? [:created_at, :representative_id, :employer_state, :id, :business_sequence_number] }.compact
     @representative = Representative.find_by(id: params[:representative_id])
     redirect_to page_not_found_path and return unless @representative.present?
 
-    @field_names  = EmployerDemographic.column_names.reject { |col| col if col.to_sym.in? [:created_at, :representative_id, :employer_state, :id, :business_sequence_number] }.compact
-    @demographics = @representative.employer_demographics.order(:policy_number)
+    # @demographics = @representative.employer_demographics.page(params[:page]).per(25)
+    @demographics = @representative.employer_demographics
   end
 end
